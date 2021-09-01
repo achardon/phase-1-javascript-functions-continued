dad772b2104df5661c5237cd7779ea569d014130
Alessandra Chardon <achardon@ucla.edu>
2021-09-01 09:29:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (location = 'go to the office') {
+    return `This Monday, I will ${location}.`
+}
+
+function wrapAdjective (visualFlair = '*') {
+    return function inner (anyWord = 'special') {
+        return `You are ${visualFlair}${anyWord}${visualFlair}!`
+    }
+}
